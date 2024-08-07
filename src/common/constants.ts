0678f9d2d63aa1854ee3fb29ff2fc6c34aed1512
Boris Jankovic <bjankovic998@gmail.com>
2024-08-07 13:47:51 -0700

    Changes

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -10,9 +10,3 @@ export const GOOGLE_CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID ?? '';
 export const GOOGLE_OAUTH_REDIRECT_URI =
   process.env.REACT_APP_GOOGLE_OAUTH_REDIRECT_URI ?? 'http://localhost:3000';
 export const COOKIE_DOMAIN = process.env.REACT_APP_COOKIE_DOMAIN ?? '';
-
-console.log('IS_PROD', IS_PROD);
-console.log('API_BASE_URL', API_BASE_URL);
-console.log('GOOGLE_CLIENT_ID', GOOGLE_CLIENT_ID);
-console.log('GOOGLE_OAUTH_REDIRECT_URI', GOOGLE_OAUTH_REDIRECT_URI);
-console.log('COOKIE_DOMAIN', COOKIE_DOMAIN);
