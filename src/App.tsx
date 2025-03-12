1523de573c1ea2cff9b0a911bcfa5fad713ff999
Boris Jankovic <bjankovic998@gmail.com>
2025-03-12 15:45:36 +0100

    Update VSCode settings for TypeScript React formatting and refactor App component for consistent import style

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import { GoogleOAuthProvider } from "@react-oauth/google";
-import { Toaster } from "sonner";
-import { ApiProvider } from "./global/providers/api-provider";
-import { Router } from "./global/router/router";
-import { GOOGLE_CLIENT_ID } from "./common/constants";
+import { GoogleOAuthProvider } from '@react-oauth/google'
+import { Toaster } from 'sonner'
+import { ApiProvider } from './global/providers/api-provider'
+import { Router } from './global/router/router'
+import { GOOGLE_CLIENT_ID } from './common/constants'
 
 export const App = () => {
   return (
     <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
       <ApiProvider>
         <Router />
-        <Toaster position="bottom-right" />
+        <Toaster position='bottom-right' />
       </ApiProvider>
     </GoogleOAuthProvider>
-  );
-};
+  )
+}
